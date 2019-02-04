af826e1faff20481bbf89d5df4898f284c7b6f0b
Kevin <kevinvr@hotmail.es>
2019-02-04 00:03:36 -0600

    Inicio Funcionalidad

--- function.js
+++ function.js
@@ -2,6 +2,6 @@ function inicial() {
 	var campoInit = document.getElementById('preciod');
 	var campoFin = document.getElementById('preciop')
 	var incr = document.getElementById('porcentaje');
-	var precioFin = ((incr.value/100)+1)*precioInit.value;
+	var precioFin = ((incr.value/100)+1)*campoInit.value;
 	campoFin.value = precioFin;
 }
